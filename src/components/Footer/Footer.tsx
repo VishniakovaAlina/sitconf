4cfa79f45f97e10d9b00956af05458c4982a5804
Alina Vishniakova <alinavishniakova@Alinas-MacBook-Pro-8.local>
2025-03-11 19:45:42 +0400

    added main block

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,9 @@
+import TGIcon from '@public/img/tg.svg';
+import VKIcon from '@public/img/vk.svg';
+import YoutubeIcon from '@public/img/youtube.svg';
+
 import { ContentContainer } from '@/components/ContentContainer/ContentContainer';
+import { Logo } from '@/components/Logo';
 
 import styles from './Footer.module.scss';
 
@@ -6,10 +11,27 @@ interface IFooterProps {
   className?: string;
 }
 
+export const YoutubeLink = 'https://www.youtube.com/@SamaraITCommunity';
+export const VKLink = 'https://vk.com/samara_it_community';
+export const TGLink = 'https://t.me/Samara_IT_Community';
+
 export const Footer = ({ className }: IFooterProps) => {
   return (
     <section className={cn(styles.root, className)}>
-      <ContentContainer className={styles.container} />
+      <ContentContainer className={styles.container}>
+        <Logo className={styles.logo} />
+        <div className={styles.links}>
+          <a href={YoutubeLink}>
+            <YoutubeIcon />
+          </a>
+          <a href={VKLink}>
+            <VKIcon />
+          </a>
+          <a href={TGLink}>
+            <TGIcon />
+          </a>
+        </div>
+      </ContentContainer>
     </section>
   );
 };
--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,11 +1,43 @@
+import Image from 'next/image';
 import { MainLayout } from 'src/layouts/Main';
 
+import MainImage from '@public/img/main_image.png';
+
+import { Button } from '@/components/Button/Button';
+import { ContentContainer } from '@/components/ContentContainer/ContentContainer';
 import { meta } from '@/constants/meta';
+
+import styles from './HomePage.module.scss';
 
 const HomePage = () => {
   const { title, description } = meta.home;
 
-  return <MainLayout title={title} description={description} />;
+  return (
+    <MainLayout title={title} description={description} className={styles.root}>
+      <ContentContainer className={styles.container}>
+        <div className={styles.title}>
+          <div>
+            <h4>26 октября</h4>
+            <h4>Коворкинг YellowRockets</h4>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <h1>
+            IT конференция
+            <br /> для лучших людей
+          </h1>
+          <p>
+            IT-конференция в Самаре, созданная сообществом SITC. Она посвящена актуальным темам и трендам в IT-индустрии и служит местом
+            встречи для обмена опытом с единомышленниками и установления профессиональных контактов
+          </p>
+        </div>
+        <Image src={MainImage} alt="sitconf" />
+      </ContentContainer>
+      <ContentContainer>
+        <Button>хочу быть спикером</Button>
+      </ContentContainer>
+    </MainLayout>
+  );
 };
 
 export default HomePage;
