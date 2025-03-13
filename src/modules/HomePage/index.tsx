eb71d0fe7380205de59fa6675fa46c21238428db
ainura <annasejdalieva@gmail.com>
2025-03-13 20:02:28 +0400

    feat: adds conferences blocks

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -4,6 +4,7 @@ import { MainLayout } from 'src/layouts/Main';
 import MainImage from '@public/img/main_image.png';
 
 import { Button } from '@/components/Button/Button';
+import { Conference } from '@/components/Conference/Conference';
 import { ContentContainer } from '@/components/ContentContainer/ContentContainer';
 import { meta } from '@/constants/meta';
 
@@ -32,6 +33,58 @@ const HomePage = () => {
           </p>
         </div>
         <Image src={MainImage} alt="sitconf" />
+      </ContentContainer>
+      <ContentContainer>
+        <div className={styles.conferences}>
+          <div className={styles.blue_line}>
+            <h4>Предстоящие конференции</h4>
+          </div>
+          <div className={styles.conferences_container}>
+            <Conference
+              name="Management"
+              date="26 октября"
+              place="Коворкинг Yellow Rockets"
+              description="SITConf Management - это конференция для руководителей, тимлидов и тех, кто стремится ими стать. Здесь вы сможете обменяться
+              опытом с коллегами и познакомиться с крутанами"
+              link="#"
+            />
+            <Conference
+              name="QA"
+              date="26 октября"
+              place="Коворкинг Yellow Rockets"
+              description="SITConf QA - это конференция QA-инженеров, автотестеров и всех, кто стремится глубже погрузиться в мир качества. Здесь вы
+              можете обменяться опытом с коллегами"
+              link="#"
+            />
+          </div>
+          <div className={styles.blue_line} />
+        </div>
+      </ContentContainer>
+      <ContentContainer>
+        <div className={styles.conferences}>
+          <div className={styles.blue_line}>
+            <h4>Прошедшие конференции</h4>
+          </div>
+          <div className={styles.conferences_container}>
+            <Conference
+              name="Management"
+              date="26 октября"
+              place="Коворкинг Yellow Rockets"
+              description="SITConf Management - это конференция для руководителей, тимлидов и тех, кто стремится ими стать. Здесь вы сможете обменяться
+              опытом с коллегами и познакомиться с крутанами"
+              link="#"
+            />
+            <Conference
+              name="QA"
+              date="26 октября"
+              place="Коворкинг Yellow Rockets"
+              description="SITConf QA - это конференция QA-инженеров, автотестеров и всех, кто стремится глубже погрузиться в мир качества. Здесь вы
+              можете обменяться опытом с коллегами"
+              link="#"
+            />
+          </div>
+          <div className={styles.blue_line} />
+        </div>
       </ContentContainer>
       <ContentContainer>
         <Button>хочу быть спикером</Button>
